39ea292caeb904473ea0e419ee8158321d3b54d5
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-08-30 09:37:50 -0300

    feat: ajustment from deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://plannernodeapi.onrender.com', // Seu back-end real
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Reescreve a URL, removendo '/api' do início do caminho
+      },
+    },
+  },
 })
