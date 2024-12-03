b72cb9e05d1f4ffcfac2f59cc88a412654904c3f
PedroHenrique1606 <pedromelo.dev.contato@gmail.com>
2024-12-03 16:38:16 -0300

    feat: ajustment links backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Cria uma instância do axios com a URL base, utilizando variáveis de ambiente
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || "https://plannernodeapi.onrender.com",
+    baseURL: import.meta.env.BACKEND_LINK || "https://plannernodeapi.onrender.com",
 });
 
 export default api;
